fd3a8ceded0c89f0284ce80a7c8bf4d916dcae2f
Попов Федор Павлович <eynsherkiel@gmail.com>
2019-10-27 16:46:32 +0300

    Temp changes

--- StoryTale/ClientApp/src/App.tsx
+++ StoryTale/ClientApp/src/App.tsx
@@ -1,6 +1,32 @@
 import * as React from 'react'; 
 import { Flow } from './Graph/Flow';
+import style from "./App.module.css"
 
-const App: React.FC = () => (<Flow/>);
+const App: React.FC = () => (
+    <div>
+        <div className={style.box}>
+            <div>
+                <button>1</button>
+            </div>
+            <div>
+                <button>2</button>
+            </div>
+            <div>
+                <button>3</button>
+            </div>
+            <div>
+                <button>4</button>
+            </div>
+            <div className={style.flow}>
+                <Flow />
+            </div>
+        </div>
+
+        <div className={style.r}>
+            dsada
+        </div>
+
+    </div>
+);
 
 export default App;
