5d11f3c3068f5696d19efb151e4a60dccf08fe0a
Попов Федор Павлович <eynsherkiel@gmail.com>
2019-10-27 23:38:10 +0300

    change viewer to react-digraph

--- StoryTale/ClientApp/src/App.tsx
+++ StoryTale/ClientApp/src/App.tsx
@@ -4,28 +4,9 @@ import style from "./App.module.css"
 
 const App: React.FC = () => (
     <div>
-        <div className={style.box}>
-            <div>
-                <button>1</button>
-            </div>
-            <div>
-                <button>2</button>
-            </div>
-            <div>
-                <button>3</button>
-            </div>
-            <div>
-                <button>4</button>
-            </div>
-            <div className={style.flow}>
-                <Flow />
-            </div>
+        <div className={style.block}>
+            <Flow />
         </div>
-
-        <div className={style.r}>
-            dsada
-        </div>
-
     </div>
 );
 
